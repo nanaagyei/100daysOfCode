e5ca9be846bbbea1ccb62ea06740d5a1aedf25bb
Prince Agyei Tuffour <44074440+nanaagyei@users.noreply.github.com>
2023-07-21 12:38:57 -0700

    Delete day4.py

--- day4.py
+++
@@ -1,7 +0,0 @@
-print("This program only finds the sum of multiples of three and five for n consecutive integers")
-n=input("Please the number of consecutive integers: ")
-total = 0
-for i in range(1, n+1):
-    if i//3==0 or i//5==0:
-        total += i
-print(total)
