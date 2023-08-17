423329e0b57f95335a9b0304c2c9c27d2b631eb4
nanaagyei <tuffourp@oregonstate.edu>
2023-08-17 01:45:58 -0700

    day34 quizapp with tkinter/opentriviaAPI

--- Day34/ui.py
+++ Day34/ui.py
@@ -26,11 +26,15 @@ class QuizUI:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=30, padx=4)
 
         self.true_img = PhotoImage(file="images/true.png")
-        self.true_btn = Button(image=self.true_img, highlightthickness=0, highlightbackground=THEME_COLOR)
+        self.true_btn = Button(image=self.true_img, highlightthickness=0,
+                               highlightbackground=THEME_COLOR,
+                               command=self.true)
         self.true_btn.grid(column=0, row=2)
 
         self.false_img = PhotoImage(file="images/false.png")
-        self.false_btn = Button(image=self.false_img, highlightthickness=0, highlightbackground=THEME_COLOR)
+        self.false_btn = Button(image=self.false_img, highlightthickness=0,
+                                highlightbackground=THEME_COLOR,
+                                command=self.false)
         self.false_btn.grid(column=1, row=2)
 
         self.get_next_question()
@@ -38,6 +42,27 @@ class QuizUI:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.q_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_text.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.q_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.q_text, text=f"End of quiz! You scored {self.quiz.score} out of 10")
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
+
+    def true(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
 
