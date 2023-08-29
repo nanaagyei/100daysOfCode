1f836961448b2e8f88ec3a49d4a8e1a6f4a2f681
nanaagyei <tuffourp@oregonstate.edu>
2023-08-29 15:07:13 -0700

    Amazon price deal alert demo

--- Day47AmazonPriceAlert/main.py
+++ Day47AmazonPriceAlert/main.py
@@ -1,7 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import os
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+MY_EMAIL = "prince.agyei.tuffour@gmail.com"
+MY_PASSWORD = os.environ["MY_PASSWORD"]
+RECIPIENT_ADDRESS = "nanakwameagyeituffour@gmail.com"
 
 AMAZON_URL = "https://www.amazon.com/dp/B0BCPCSTNB/ref=sspa_dk_detail_" \
              "2?psc=1&pd_rd_i=B0BCPCSTNB&pd_rd_w=QYzE1&content-id=amzn1.sym." \
@@ -23,5 +30,32 @@ amazon_webpage = amazon_webpage_response.text
 
 soup = BeautifulSoup(amazon_webpage, "lxml")
 item_price_html = soup.find(name="span", class_="a-offscreen")
+product_name_html = soup.find(name="span", class_="product-title-word-break")
+product_name = product_name_html.getText()
+product_title = product_name.strip()
 item_price = item_price_html.getText()[1:]
 item_price = float(item_price)
+
+target_price = 114.99
+
+if item_price < target_price:
+    message = f"{product_title} is now ${item_price}. You can purchase at:\n{AMAZON_URL}"
+    msg = MIMEMultipart()
+    msg['From'] = MY_EMAIL
+    msg['To'] = RECIPIENT_ADDRESS
+    msg['Subject'] = "Amazon Price Alert"
+    msg.attach(MIMEText(message, 'plain', 'utf-8'))
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+
+        connection.starttls()
+        connection.login(
+            user=MY_EMAIL,
+            password=MY_PASSWORD
+        )
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=RECIPIENT_ADDRESS,
+            msg=msg.as_string()
+        )
+
