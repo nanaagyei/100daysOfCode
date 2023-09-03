9254842816ca324f19a0e12dbd10cbfee0003ec5
nanaagyei <tuffourp@oregonstate.edu>
2023-09-03 00:50:40 -0700

    final

--- Day53/main.py
+++ Day53/main.py
@@ -17,3 +17,57 @@ response = requests.get(
 data = response.text
 soup = BeautifulSoup(data, "html.parser")
 
+all_link_elements = soup.select(".list-card-top a")
+
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    print(href)
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".list-card-heading")
+all_prices = []
+for element in all_price_elements:
+    # Get the prices. Single and multiple listings have different tag & class structures
+    try:
+        # Price with only one listing
+        price = element.select(".list-card-price")[0].contents[0]
+    except IndexError:
+        print('Multiple listings for the card')
+        # Price with multiple listings
+        price = element.select(".list-card-details li")[0].contents[0]
+    finally:
+        all_prices.append(price)
+
+
+# Create Spreadsheet using Google Form
+# Substitute your own path here 👇
+# chrome_driver_path = YOUR_PATH_HERE
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for n in range(len(all_links)):
+    # Substitute your own Google Form URL here 👇
+    driver.get("https://forms.gle/LweXsngq2SYTiZac6")
+
+    time.sleep(2)
+    address = driver.find_element(By.XPATH,
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH,
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH,
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
+
