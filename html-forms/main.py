04744c5c4bf562dc580ebed1441208089f06c6df
nanaagyei <tuffourp@oregonstate.edu>
2023-09-08 18:54:20 -0700

    working contact form for blog post

--- html-forms/main.py
+++ html-forms/main.py
@@ -1,5 +1,13 @@
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from flask import Flask, render_template, request
 import requests
+import smtplib
+
+MY_EMAIL = "prince.agyei.tuffour@gmail.com"
+MY_PASSWORD = os.environ["MY_PASSWORD"]
+RECIPIENT = "nanakwameagyeituffour@gmail.com"
 
 posts = requests.get("https://api.npoint.io/2ad8ecaddae227b32703").json()
 
@@ -19,21 +27,31 @@ def about():
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form["name"])
-        print(request.form["email"])
-        print(request.form["phone"])
-        print(request.form["message"])
-        return "<h1>Successfully sent your message.</h1>"
-    return render_template("contact.html")
+        data = request.form
+        send_msg(data['name'], data['email'], data['phone'], data['message'])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
-# @app.route("/form-entry", methods=["POST"])
-# def receive_data():
-#     print(request.form["name"])
-#     print(request.form["email"])
-#     print(request.form["phone"])
-#     print(request.form["message"])
-#     return "<h1>Successfully sent your message.</h1>"
+def send_msg(name, email, phone, message):
+    message = f"Name: {name}\n" \
+              f"Email: {email}\n" \
+              f"Phone: {phone}\n" \
+              f"Message: {message}"
+    msg = MIMEMultipart()
+    msg['From'] = MY_EMAIL
+    msg['To'] = RECIPIENT
+    msg['Subject'] = "New flight deal alert!"
+    msg.attach(MIMEText(message, 'plain', 'utf-8'))  # Use 'plain' for plain text email
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=RECIPIENT,
+            msg=msg.as_string()  # Convert the MIMEText object to a string
+        )
 
 
 @app.route("/post/<int:index>")
