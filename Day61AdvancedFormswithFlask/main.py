ceee8fb777d48b66eae98e2fbd09ba8b032188cd
nanaagyei <tuffourp@oregonstate.edu>
2023-09-11 22:26:34 -0700

    update forms

--- Day61AdvancedFormswithFlask/main.py
+++ Day61AdvancedFormswithFlask/main.py
@@ -1,25 +1,29 @@
+import os
 from flask import Flask, render_template
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
+
+
+class MyForm(FlaskForm):
+    email = EmailField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired(), Length()])
+    submit = SubmitField(label="Log In")
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = MyForm()
+    form.validate_on_submit()
+    return render_template("login.html", form=form)
 
 
 if __name__ == '__main__':
