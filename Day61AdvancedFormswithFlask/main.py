f90677791a37506e0ccb884714d7aa6a8fa98029
nanaagyei <tuffourp@oregonstate.edu>
2023-09-11 22:59:04 -0700

    update forms

--- Day61AdvancedFormswithFlask/main.py
+++ Day61AdvancedFormswithFlask/main.py
@@ -9,8 +9,16 @@ app.secret_key = os.urandom(16)
 
 
 class MyForm(FlaskForm):
-    email = EmailField(label="Email", validators=[DataRequired(), Email()])
-    password = PasswordField(label="Password", validators=[DataRequired(), Length()])
+    email = EmailField(label="Email",
+                       validators=[DataRequired(),
+                                   Email(message="Invalid email address.")
+                                   ]
+                       )
+    password = PasswordField(label="Password",
+                             validators=[DataRequired(),
+                                         Length(min=8, message="Field must be at least 8 characters long.")
+                                         ]
+                             )
     submit = SubmitField(label="Log In")
 
 
@@ -22,7 +30,11 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     form = MyForm()
-    form.validate_on_submit()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=form)
 
 
